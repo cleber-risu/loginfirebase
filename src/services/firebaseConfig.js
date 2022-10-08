0581490e614e0fb40ea870522e3e4f6e04caa084
Cleber <cleber.r.sudul@gmail.com>
2022-10-08 19:25:11 -0300

    Add config firebase

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAY4_ayQmLaefCyR2U7JWTdlwo8Hh8x6zQ",
+  authDomain: "loginfirebase-54dde.firebaseapp.com",
+  projectId: "loginfirebase-54dde",
+  storageBucket: "loginfirebase-54dde.appspot.com",
+  messagingSenderId: "631806617044",
+  appId: "1:631806617044:web:e6fa8b74529e307db1e241",
+  measurementId: "G-D3KQ1V70QC"
+};
+
+export const app = initializeApp(firebaseConfig);
+
