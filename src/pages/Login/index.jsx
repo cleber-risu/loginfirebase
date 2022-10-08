ea242967a83c380bf7ac82dee7361d0570072133
Cleber <cleber.r.sudul@gmail.com>
2022-10-08 19:40:25 -0300

    Add Login Google

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,35 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../../services/firebaseConfig";
+
+const provider = new GoogleAuthProvider();
+
 const Login = () => {
+  const signInGoogle = () => {
+    const auth = getAuth(app);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   return (
     <div>
-      <h1>Login</h1>
+      <button onClick={signInGoogle}>Logar com o Google</button>
     </div>
   );
 };
